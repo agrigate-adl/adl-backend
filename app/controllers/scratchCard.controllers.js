c7704b8f354cd51416679682fbf766dbae2c6cca
khaifan-kfan <khalifanmuwonge@gmail.com>
2023-06-09 17:41:27 -0700

    add card search

--- app/controllers/scratchCard.controllers.js
+++ app/controllers/scratchCard.controllers.js
@@ -35,6 +35,23 @@ sc.insertMany(insertationArray).then((data) => {
       message: "failed to insert cards",
     });
    });
+  //save cards record
+}
+exports.getCard  = async (req, res)=>{
+  const val = req.params.val;
+  sc.findOne({ cardNumber: val })
+    .then((data) => {
+      if (data !== null) {
+        res.status(200).send({ message: "success", data: data });
+      } else {
+        res.status(404).send({ message: "no card retrieved", data: null });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "error, can't retrieve data",
+      });
+    });
 }
 
 exports.getCardAvailableCounts = async (req, res) => {
@@ -90,62 +107,94 @@ exports.getCardUsedCounts = async (req, res) => {
 }
 
 exports.overAllCount = async (req, res) => {
-        sc.aggregate([
-            { "$facet": {
-              "Used": [
-                { "$match" : {  "status": "used" }},
-                { "$count": "Used" },
-              ],
-              "Unused": [
-                { "$match" : {  "status": "unused" }},
-                { "$count": "Unused" }
-              ],
-              "C500": [
-                { "$match" : {"$and": [ {"amount": "500" },{"status":"used"}]}},
-                { "$count": "C500" }
-              ],
-              "C1000": [
-                { "$match" : {"$and": [ {"amount": "1000" },{"status":"used"}]}},
-                { "$count": "C1000" }
-              ],
-              "C2000": [
-                { "$match" : {"$and": [ {"amount": "2000" },{"status":"used"}]}},
-                { "$count": "C2000" }
-              ],
-              "C5000": [
-                { "$match" : {"$and": [ {"amount": "5000" },{"status":"used"}]}},
-                { "$count": "C5000" }
-              ],
-              "C10000": [
-                { "$match" : {"$and": [ {"amount": "10000" },{"status":"used"}]}},
-                { "$count": "C10000" }
-              ],
-              "C20000": [
-                { "$match" : {"$and": [ {"amount": "20000" },{"status":"used"}]}},
-                { "$count": "C20000" }
-              ],
-              "C50000": [
-                { "$match" : {"$and": [ {"amount": "50000" },{"status":"used"}]}},
-                { "$count": "C50000" }
-              ],
-            }},
-            { "$project": {
-              "Used": { "$arrayElemAt": ["$Used.Used", 0] },
-              "Unused": { "$arrayElemAt": ["$Unused.Unused", 0] },
-              "C500": { "$arrayElemAt": ["$C500.C500", 0] },
-              "C1000": { "$arrayElemAt": ["$C1000.C1000", 0] },
-              "C2000": { "$arrayElemAt": ["$C2000.C2000", 0] },
-              "C5000": { "$arrayElemAt": ["$C5000.C5000", 0] },
-              "C10000": { "$arrayElemAt": ["$C10000.C10000", 0] },
-              "C20000": { "$arrayElemAt": ["$C20000.C20000", 0] },
-              "C50000": { "$arrayElemAt": ["$C50000.C50000", 0] },
-            }}
-          ])
-            .then((count)=>{
-                console.log(count)
-                return res.status(200).send({message:"success",count});
-            }).catch((e)=>{
-                return res.status(430).send({message:"failed to get count"});
-            })
+  try {
+    const counts = await sc.aggregate([
+      {
+        $facet: {
+          Used: [
+            { $match: { status: "used" } },
+            { $count: "count" },
+          ],
+          Unused: [
+            { $match: { status: "unused" } },
+            { $count: "count" },
+          ],
+          C500: [
+            {
+              $match: {
+                $and: [{ amount: "500" }, { status: "used" }],
+              },
+            },
+            { $count: "count" },
+          ],
+          C1000: [
+            {
+              $match: {
+                $and: [{ amount: "1000" }, { status: "used" }],
+              },
+            },
+            { $count: "count" },
+          ],
+          C2000: [
+            {
+              $match: {
+                $and: [{ amount: "2000" }, { status: "used" }],
+              },
+            },
+            { $count: "count" },
+          ],
+          C5000: [
+            {
+              $match: {
+                $and: [{ amount: "5000" }, { status: "used" }],
+              },
+            },
+            { $count: "count" },
+          ],
+          C10000: [
+            {
+              $match: {
+                $and: [{ amount: "10000" }, { status: "used" }],
+              },
+            },
+            { $count: "count" },
+          ],
+          C20000: [
+            {
+              $match: {
+                $and: [{ amount: "20000" }, { status: "used" }],
+              },
+            },
+            { $count: "count" },
+          ],
+          C50000: [
+            {
+              $match: {
+                $and: [{ amount: "50000" }, { status: "used" }],
+              },
+            },
+            { $count: "count" },
+          ],
+        },
+      },
+    ]);
     
-}
+    const formattedCounts = counts[0]; // Access the first element of the array
+    console.log(formattedCounts)
+    const result = {
+      Used: formattedCounts.Used[0].count || 0,
+      Unused: formattedCounts.Unused[0].count || 0,
+      C500: formattedCounts.C500[0].count || 0,
+      C1000: formattedCounts.C1000[0].count || 0,
+      C2000: formattedCounts.C2000[0].count || 0,
+      C5000: formattedCounts.C5000[0].count || 0,
+      C10000: formattedCounts.C10000[0].count || 0,
+      C20000: formattedCounts.C20000[0].count || 0,
+      C50000: formattedCounts.C50000[0].count || 0,
+    };
+
+    return res.status(200).send({ message: "success", count: result });
+  } catch (error) {
+    return res.status(430).send({ message: "failed to get count" });
+  }
+};
--- app/routes/scratchcards.routes.js
+++ app/routes/scratchcards.routes.js
@@ -6,6 +6,7 @@ module.exports = app => {
     var router = require("express").Router(); 
     
     router.post("/print-newcards",scratchCard.addCardBatchs);
+    router.get("/:val",scratchCard.getCard);
     router.post("/available",scratchCard.getCardAvailableCounts);
     router.post("/unavailable",scratchCard.getCardUsedCounts);
     router.get("/pro-count",scratchCard.overAllCount);
